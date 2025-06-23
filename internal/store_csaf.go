d27f55b68eb2d3e865c096556ce2075f5dadf3d3
Nico Haas <nico.haas@aisec.fraunhofer.de>
2025-06-23 11:51:24 +0200

    Fix method parameter type

--- internal/store_csaf.go
+++ internal/store_csaf.go
@@ -3,6 +3,6 @@ package internal
 import "github.com/csaf-poc/ghsa/models/csaf"
 
 // TODO(lebogg): Implement
-func StoreCSAF(csaf csaf.CSAF) error {
+func StoreCSAF(csaf csaf.Document) error {
 	return nil
 }
