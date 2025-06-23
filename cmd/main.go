b66fe6b8b86c6e79f25290ed80324ae2d859056c
Nico Haas <nico.haas@aisec.fraunhofer.de>
2025-06-23 11:46:18 +0200

    Add skeleton for main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,12 @@
 package main
+
+// TODO(lebogg): Implement entrypoint: URL of GHSA as argument |
+func main() {
+	// Check arguments
+
+	// Get GHSA
+
+	// Convert GHSA to CSAF
+
+	// Store CSAF
+}
