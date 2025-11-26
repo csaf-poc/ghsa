febdfdedae666a5c90ba4276f7986097fe390c02
Nico Haas <nico.haas@aisec.fraunhofer.de>
2025-11-26 09:16:56 +0100

    ProductTree: Add category and TODOs

--- service/converter/producttree.go
+++ service/converter/producttree.go
@@ -7,12 +7,13 @@ import (
 	"github.com/csaf-poc/ghsa/internal/utils"
 	"github.com/csaf-poc/ghsa/models/csaf"
 	"github.com/csaf-poc/ghsa/models/ghsa/repository"
+
 	gocsaf "github.com/gocsaf/csaf/v3/csaf"
 )
 
 // TODO(lebogg): Implement
 // getProductTree converts GHSA vulnerability information into a CSAF product tree.
-// It builds a hierarchical structure: vendor -> product_name -> product_version
+// It builds a hierarchical structure: vendor -> product_name -> product_version_range
 // for each vulnerable package and version range in the advisory.
 func getProductTree(adv *repository.Advisory) (pt *csaf.ProductTree, err error) {
 	// In a GHSA, vulnerabilities represent affected packages along with their versions
@@ -23,10 +24,12 @@ func getProductTree(adv *repository.Advisory) (pt *csaf.ProductTree, err error) 
 	for _, v := range adv.Vulnerabilities {
 		branch := &gocsaf.Branch{
 			// 1st) add ecosystem branch, 2nd) add product branch and 3rd) add version range
+			// Note: CSAF only allows a branch to EITHER have a branch OR a product
+			// TODO(lebogg): What is the right category here? Language is probably no programming language but would be fit here. Vendor would be, e.g., GitHub or the Package Owner.
+			Category: utils.Ref(gocsaf.CSAFBranchCategoryLanguage),
+			Name:     utils.Ref(v.Package.Ecosystem),
 			Branches: []*gocsaf.Branch{
 				{
-					Category: utils.Ref(gocsaf.BranchCategory("TODO")),
-					Name:     utils.Ref(v.Package.Ecosystem),
 					Branches: []*gocsaf.Branch{
 						{
 							Category: utils.Ref(gocsaf.CSAFBranchCategoryProductName),
@@ -45,12 +48,6 @@ func getProductTree(adv *repository.Advisory) (pt *csaf.ProductTree, err error) 
 					},
 				},
 			},
-			Category: utils.Ref(gocsaf.CSAFBranchCategoryProductVersionRange),
-			Name:     utils.Ref(v.VulnerableVersionRange),
-			Product: &gocsaf.FullProductName{
-				Name:      nil,
-				ProductID: utils.Ref(gocsaf.ProductID(v.Package.Name)),
-			},
 		}
 		branches = append(pt.Branches, branch)
 	}
@@ -60,6 +57,7 @@ func getProductTree(adv *repository.Advisory) (pt *csaf.ProductTree, err error) 
 
 // getRepositoryName gets the repository name out of the package name.
 // For example: "github.com/golang-jwt/jwt/v5" -> "jwt"
+// TODO(lebogg): Check if there are other GitHub URL constellations
 func getRepositoryName(packageName string) *string {
 	splits := strings.Split(packageName, "/")
 	if len(splits) > 2 {
