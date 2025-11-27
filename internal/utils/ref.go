a0f0149c173b9b214133833bd587c06556babd8e
Nico Haas <nico.haas@aisec.fraunhofer.de>
2025-11-27 10:23:57 +0100

    Add Deref

--- internal/utils/ref.go
+++ internal/utils/ref.go
@@ -3,3 +3,10 @@ package utils
 func Ref[T any](v T) *T {
 	return &v
 }
+
+func Deref[T any](v *T) (result T) {
+	if v != nil {
+		result = *v
+	}
+	return
+}
--- service/store/store_csaf.go
+++ service/store/store_csaf.go
@@ -3,13 +3,14 @@ package store
 import (
 	"log/slog"
 
+	"github.com/csaf-poc/ghsa/internal/utils"
 	"github.com/csaf-poc/ghsa/models/csaf"
 	gocsaf "github.com/gocsaf/csaf/v3/csaf"
 )
 
 func Save(adv *csaf.Advisory, fname string) (err error) {
 	slog.Info("Saving advisory",
-		slog.Any("CSAF advisory", adv), // TODO(lebogg): Check that this does not explode since too much info
+		slog.Any("CSAF advisory", utils.Deref(adv.Document.Title)),
 		slog.String("file name", fname))
 	err = gocsaf.SaveAdvisory(adv, fname)
 	if err != nil {
